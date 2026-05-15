632f0161f5438ac0c671e8b03426928d5a103e91
kaushal kapupara <kaushal120804@gmail.com>
2026-05-15 12:28:22 +0530

    Website Updates

--- NotesApp/settings.py
+++ NotesApp/settings.py
@@ -116,7 +116,6 @@ USE_TZ = True
 
 
 # Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/4.2/howto/static-files/
 STATIC_URL = 'static/'
 STATICFILES_DIRS = [BASE_DIR / 'static']
 STATIC_ROOT = BASE_DIR / 'staticfiles'
@@ -150,4 +149,3 @@ STRIPE_WEBHOOK_SECRET = 'whsec_your_webhook_secret'
 
 # Change these to your actual domain in production
 SITE_URL = 'https://notesapp1284.pythonanywhere.com'
-STATIC_ROOT = BASE_DIR / 'staticfiles'
